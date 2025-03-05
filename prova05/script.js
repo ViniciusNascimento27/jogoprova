ff921a80cdcdce63fdeed6e28e611a8839a92a8e
VINICIUS SCHÜRHAUS DO NASCIMENTO <vinicius_nascimen5@estudante.sesisenai.org.br>
2025-03-05 15:17:42 -0300

    atualizacao

---
+++ prova05/script.js
@@ -0,0 +1,23 @@
+const tela = document.getElementById("telaJogo");
+const ctx = tela.getContext("2d");
+const pontuancaoTextto = document.getElementById("pontuacao");
+const fimDeJogoTexto = document.getElementById("fimDeJogo");
+const botaoReiniciar = document.getElementById("botaoReiniciar")
+let pontuacao = 0;
+fimDeJogo = false;
+
+class Raquete {
+    constructor() {
+        this.largura = 100;
+        this.altura = 10;
+        this.x = (tela.width - this.largura) /2;
+        this.y = tela.height - this.altura - 10;
+        this.velocidade = 8;
+        this.dx = 0
+    }
+    
+    desenhar() {
+        ctx.fillStyle = "blue";
+        ctx.fillRect(this.x, this.y, this.largura)
+    }
+}
